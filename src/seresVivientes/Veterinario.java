b8f94293491b8fc3c07897c9652e817db5ae0bdb
Cristian Camilo Henao Rojas <41649200+Tholes@users.noreply.github.com>
2019-06-14 20:59:58 -0500

    Atributos seres
    Añadí los atributos (vacíos, sin funcionalidad) de las clases Cliente, Paciente y Veterinario, están muy susceptibles a cambios.

--- src/seresVivientes/Veterinario.java
+++ src/seresVivientes/Veterinario.java
@@ -1,14 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package seresVivientes;
 
-/**
- *
- * @author CristianH
- */
 public class Veterinario {
+
+    private int id;
+    private String nombre;
+    private String email;
+    private String direccion;
+    private String cargo;
+    private String especialidad;
+    private int experiencia;
+    private int sueldo;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public int getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(int sueldo) {
+        this.sueldo = sueldo;
+    }
     
 }
