f730bdec1f4011114da4253c1abbd74efe7f1a38
Mayra Tejada <44760892+MayraTejada@users.noreply.github.com>
2019-06-20 15:55:11 -0500

    eliminé esta clase de mi carpeta.

--- src/servicio/Consulta.java
+++
@@ -1,9 +0,0 @@
-package servicio;
-import seresVivientes.*;
-import java.util.ArrayList;
-import finanzas.*;
-public class Consulta {
-    private ArrayList<Historial>historial=new ArrayList<>();
-    private ArrayList<Veterinario>veterinario=new ArrayList<>();
-    private ArrayList<Servicio>servicio=new ArrayList<>();
-}
