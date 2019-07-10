73ae2b19244a96e4472eb4f27c1dd8ab187977fe
Mayra Tejada <mptejadao@unal.edu.co>
2019-07-09 23:13:21 -0500

    //me falta agregar el cuerpo de los métodos

--- src/Usuarios/Administrador.java
+++ src/Usuarios/Administrador.java
@@ -1,5 +1,17 @@
 package Usuarios;
 
 public class Administrador extends Persona {
+    
+    public void asignarConsultorio(Veterinario vet){
+        
+    }
+    
+    public void contratarVeterianrio(){
+        
+    }
+    
+    public void aprobarCitas(){
+        
+    }
 
 }
