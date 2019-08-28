eccdb581574c9f4336668e37c61a58076e056d6b
Mayra Tejada <44760892+MayraTejada@users.noreply.github.com>
2019-08-27 21:20:45 -0500

    Update ControlBienvenida.java

--- src/control/loginControl/ControlBienvenida.java
+++ src/control/loginControl/ControlBienvenida.java
@@ -1,1 +1,20 @@
 
+package Control;
+import java.awt.*;
+import java.awt.event.*;
+import UIMain.Main;
+public class ControlBienvenida implements MouseListener {
+	
+	public void mouseEntered(MouseEvent evento1) {
+        Main.ventanaLogin.getL1().setForeground((Color.BLUE));
+    }
+    public void mouseExited(MouseEvent evento2) {
+    	Main.ventanaLogin.getL1().setForeground((Color.BLACK));
+    }
+    public void mouseReleased(MouseEvent evento3) {
+    }
+    public void mouseClicked(MouseEvent evento3) {
+    }
+    public void mousePressed(MouseEvent evento3) {	
+    }
+}
