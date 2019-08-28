1ae00c04be12f894963a9179b2a0be78119e0e5e
Mayra Tejada <44760892+MayraTejada@users.noreply.github.com>
2019-08-27 21:19:54 -0500

    Update ControlImagenes.java

--- src/control/loginControl/ControlImagenes.java
+++ src/control/loginControl/ControlImagenes.java
@@ -1,1 +1,23 @@
+package Control;
+import java.awt.event.*;
+
+import UIMain.Main;
+
+public class ControlImagenes implements ActionListener {
+		int contador = 0;
+		public void actionPerformed(ActionEvent evento) {
+			contador++;
+			if(contador==1) {
+				Main.ventanaLogin.cambiarImagen("foto1.jpeg");
+			}
+			else if(contador==2) {
+				Main.ventanaLogin.cambiarImagen("foto2.jpeg");
+			}
+			else if(contador==3) {
+				contador = 0;
+				Main.ventanaLogin.cambiarImagen("CLICIMAGEN.png");
+			}
+		}
+} 
+
 
